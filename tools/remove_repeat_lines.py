005ef6cc0f480e274a7dc4da8d23ceb35b164174
Benjamin Fang <benjaminfang.ol@outlook.com>
2020-03-31 16:13:34 +0800

    remove repeat lines

---
+++ tools/remove_repeat_lines.py
@@ -0,0 +1,35 @@
+import sys
+a_line = [line.rstrip() for line in open(sys.argv[1])]
+
+dt = {}
+for line in a_line:
+    if line[0] == '>':
+        ff = line
+        dt[ff] = {}
+    elif line[0] == '^':
+        head = line
+        dt[ff][head] = []
+    else:
+        dt[ff][head].append(line)
+
+dt_new = {}
+for ff in dt:
+    dt_new[ff] = {}
+    for head in dt[ff]:
+        dt_new[ff][head] = []
+        block = dt[ff][head]
+        block.reverse()
+        for ele in block:
+            if block.count(ele) > 1:
+                block.remove(ele)
+        block.reverse()
+        dt_new[ff][head]=block
+
+
+for ff in dt_new:
+    print(ff)
+    for head in dt_new[ff]:
+        print(head)
+        for line in dt_new[ff][head]:
+            print(line)
+                
